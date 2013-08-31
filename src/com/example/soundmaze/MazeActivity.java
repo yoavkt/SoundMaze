2441dc7d3d8ac19a67cde3294cc36a64a0dccc11
tamar <tamar.antebi@gmail.com>
2013-08-31 15:48:36 +0300

    delete tamar

--- src/com/example/soundmaze/MazeActivity.java
+++ src/com/example/soundmaze/MazeActivity.java
@@ -44,7 +44,7 @@ public class MazeActivity extends Activity {
 				
 			}
 		});*/
-		//tttttttttttttttttttttttttttttttttttttttttttttttttttttaaaaaaaaaaaaaaaaaaaaaammmmmmmmmmmmmmmmmmmmmmmmaaaaaaaaaaaaaaaaaaaaaarrrrrrrrrrrrrrrrrrrrrrrrrrrr        
+		 
 	}
 
 	@Override
