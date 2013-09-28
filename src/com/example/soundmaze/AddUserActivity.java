6c96746c5ddcc90b746e5c4889c217a759cf5c9d
yoavkt <yoav@night-sense.com>
2013-09-28 12:09:09 +0300

    Your score is

--- src/com/example/soundmaze/AddUserActivity.java
+++ src/com/example/soundmaze/AddUserActivity.java
@@ -1,10 +1,13 @@
 package com.example.soundmaze;
 
+import com.parse.Parse;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class AddUserActivity extends Activity{
 	TableHelper th;
@@ -13,7 +16,11 @@ public class AddUserActivity extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.edit);
-		 th=new TableHelper(this);
+		
+		TextView  res = (TextView) findViewById(R.id.textViewInstruct);
+		final int score=getIntent().getIntExtra("score",0);
+		res.setText("Enter your name you scored"+String.valueOf(score)+"Points!");
+		th=new TableHelper(this);
 		findViewById(R.id.btnOK).setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				EditText userName = (EditText)findViewById(R.id.edtNewName);
@@ -22,7 +29,6 @@ public class AddUserActivity extends Activity{
 					setResult(RESULT_CANCELED);
 					finish();
 				} else {
-					int score=getIntent().getIntExtra("score",0);
 					th.insert(new User(user,score));
 					setResult(RESULT_OK);
 					finish();
